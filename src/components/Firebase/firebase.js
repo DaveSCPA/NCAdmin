b3f0514992569c64040916681ea204fe5a3fb537
Dave Smith <davesmithcpaphd@gmail.com>
2020-05-13 16:40:53 -0500

    remove api

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,14 +1,14 @@
 import app from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAJXMtdV0jcx5x2WpFvEcYoI3feStQ5Srw',
+  apiKey: 'Not available',
   authDomain: 'nc-admin-data.firebaseapp.com',
   databaseURL: 'https://nc-admin-data.firebaseio.com',
   projectId: 'nc-admin-data',
   storageBucket: 'nc-admin-data.appspot.com',
   messagingSenderId: '247022213271',
   appId: '1:247022213271:web:61c01254e90debbdd1b1d2',
-  measurementId: 'G-GLPB6V75KR'
+  measurementId: 'G-GLPB6V75KR',
 };
 
 class Firebase {
